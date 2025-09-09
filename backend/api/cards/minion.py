78075fc1e313daeabce085f0719a36f386f70f3a
reycardo <reycardo_95@hotmail.com>
2025-09-09 17:45:26 +0100

    Refactor Minion class: separate mask logic and ensure template retrieval requires masking

--- backend/api/cards/minion.py
+++ backend/api/cards/minion.py
@@ -14,16 +14,19 @@ class Minion(Card):
             is_corrupted=False,
             is_lying=True
         )
+        self.masked_card: Card = None  # To store the card being mimicked
 
-    def get_template(self, this_card_position: int, game_disposition: GameDisposition, lying: bool = True) -> str:
+    def mask(self, this_card_position: int, game_disposition: GameDisposition):
         # This card will select a random card present in game_disposition and switch its image, 
         # to the one it randomly selected and then it will use it's set_template method
         total_positions = len(game_disposition.positions)
         random.seed()
         random_position = random.choice([pos for pos in range(total_positions) if pos != this_card_position])
         random_card: Card = game_disposition.get_card_at(random_position)
+        self.masked_card = random_card
 
-        template = random_card.get_template(this_card_position=this_card_position, game_disposition=game_disposition, lying=lying)
-        self.image=random_card.image
-        self.name=random_card.name
+    def get_template(self, this_card_position: int, game_disposition: GameDisposition, lying: bool = True) -> str:
+        if not self.masked_card:
+            raise ValueError("Minion card must be masked before getting its template.")
+        template = self.masked_card.get_template(this_card_position=this_card_position, game_disposition=game_disposition, lying=lying)
         return template
