307ef0f99dd9df9bf8c845b7d8c54b20903f59eb
reycardo <reycardo_95@hotmail.com>
2025-09-16 02:04:02 +0100

    Update config values: adjust edge_padding to 80 and cardSize to 170 for improved layout

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
 // src/config.js
-export const edge_padding = 100;
+export const edge_padding = 80;
 export const skull_image = '/images/helpers/skull.png';
-export const cardSize = 100;
+export const cardSize = 170;
 export const size = 500;
